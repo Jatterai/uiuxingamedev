71f65ae37c8d0170b7b1537c311a894959a9f154
Maiorova Yuna <maiorova.yuna@gmail.com>
2023-06-19 13:00:53 +0300

    feat: add swipe slider and slider indicators

--- js/history.js
+++ js/history.js
@@ -3,21 +3,60 @@
 const slider = document.getElementById('slider')
 const leftButton = document.getElementById('left');
 const rightButton = document.getElementById('right');
+const dots = document.querySelector('.slider__dots');
 
-leftButton.addEventListener('click', (e) => {
-	e.preventDefault();
+leftButton.addEventListener('click', toLeft)
+rightButton.addEventListener('click', toRight)
+
+let touchStart = 0;
+let touchEnd = 0;
+
+function detectSwipe() {
+	if (touchStart < touchEnd) toLeft();
+	else toRight();
+}
+
+slider.addEventListener('touchstart', e => touchStart = e.changedTouches[0].clientX);
+slider.addEventListener('touchend', e => {
+	touchEnd = e.changedTouches[0].clientX;
+	detectSwipe()
+});
+
+function toLeft(e) {
+	const image = slider.style.backgroundImage;
+	let newImgNum = 0;
+	const newImage = image.replace(/\d(?=\.png)/, $1 => {
+		if ($1 > 1) {
+			newImgNum = String(+$1 - 1)
+		} else {
+			newImgNum = 6
+		}
+		return newImgNum;
+	});
+	changeActiveDot(newImgNum)
+	slider.style.backgroundImage = newImage;
+}
+
+function toRight(e) {
 
 	const image = slider.style.backgroundImage;
-	const newImage = image.replace(/\d(?=\.png)/, $1 => $1 > 1 ? String(+$1 - 1) : 6);
-
+	let newImgNum = 0;
+	const newImage = image.replace(/\d(?=\.png)/, $1 => {
+		if ($1 < 6) {
+			newImgNum = String(+$1 + 1)
+		} else {
+			newImgNum = 1;
+		}
+		return newImgNum;
+	});
 	slider.style.backgroundImage = newImage;
-})
+	changeActiveDot(newImgNum)
+}
 
-rightButton.addEventListener('click', (e) => {
-	e.preventDefault();
-
-	const image = slider.style.backgroundImage;
-	const newImage = image.replace(/\d(?=\.png)/, $1 => $1 < 6 ? String(+$1 + 1) : 1);
-
-	slider.style.backgroundImage = newImage;
-})
+function changeActiveDot(imgNum) {
+	for (let dot of dots.children) {
+		dotNum = dot.className.match(/(?<=dot)\d/);
+		dot.classList.remove('active');
+		if (+dotNum === +imgNum) dot.classList.add('active')
+	}
+}
