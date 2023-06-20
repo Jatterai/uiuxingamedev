85b8bc2d07319adde3323f28b78ffe02fbcc4261
Maiorova Yuna <maiorova.yuna@gmail.com>
2023-06-20 16:41:51 +0300

    feat: cat became a secret...

--- js/cat.js
+++ js/cat.js
@@ -1,13 +1,4 @@
-if (!localStorage.catCount) localStorage.setItem('catCount', 0);
 
-document.addEventListener('click', e => {
-	let newCount = +localStorage.catCount + 1;
-	console.log(newCount);
-
-	localStorage.catCount = newCount;
-	console.log(localStorage.catCount);
-
-})
 
 
 //============= cat ==============//
@@ -19,7 +10,6 @@ const video = document.createElement('video');
 video.src = '../assets/video/cat.webm';
 video.playsInline = true;
 
-
 video.classList.add('maxwell');
 video.loop = true;
 document.querySelector('.wrapper').prepend(video)
@@ -28,7 +18,15 @@ const moreBtns = document.querySelectorAll('._cat');
 
 moreBtns.forEach(btn => btn.addEventListener('click', maxwellPlay))
 
+
+
+
 function maxwellPlay(e) {
+	if (localStorage.alreadyKittened?.includes(e.currentTarget.id)) return;
+
+	if (!localStorage.catCount) localStorage.setItem('catCount', 0);
+	if (localStorage.catCount >= 10) return;
+	const elementId = e.currentTarget.id;
 	if (!audio.readyState && !video.readyState) {
 		audio.src = './assets/audio/Maxwell-the-Cat-Theme-djlunatique.com.mp3';
 		video.src = './assets/video/cat.webm';
@@ -48,4 +46,35 @@ function maxwellPlay(e) {
 		video.classList.remove('play')
 	}, 7300);
 
+	setTimeout(() => {
+		const newCount = Number(localStorage.catCount) + 1
+		localStorage.catCount = newCount;
+
+		if (!localStorage.alreadyKittened) {
+			localStorage.setItem('alreadyKittened', '');
+			myAlert()
+		}
+		console.log(e.currentTarget);
+
+		localStorage.alreadyKittened += ` ${elementId}`;
+	}, 7500);
+
+
+}
+
+function myAlert() {
+	const alert = document.createElement('div');
+	alert.classList.add('cat__rules');
+	alert.innerHTML = 'Поздравляю!<br>Ты нашел кота!<br>Постарайся найти все 10!';
+	const closeBtn = document.createElement('div');
+	closeBtn.innerHTML = 'X';
+	closeBtn.classList.add('cat__close')
+	alert.prepend(closeBtn);
+
+	document.querySelector('.wrapper').prepend(alert);
+
+	closeBtn.addEventListener('click', e => {
+		alert.remove();
+		closeBtn.remove();
+	});
 }
